f6e9445d3e45312af4172ee9493d81b7f911ec74
jamieliu2 <shanjliu@gmail.com>
2025-03-25 16:20:55 -0400

    pltstyle

---
+++ pltstyle.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-v0_8')
+plt.rcParams['font.family'] = 'monospace'
+plt.rcParams['lines.linewidth'] = 0.5
+plt.rcParams['lines.markersize'] = 3
