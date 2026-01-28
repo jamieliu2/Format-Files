b9c21e3dc48df3c5bbcb4d2f37b277fae7644a9e
Jamie Liu <105943280+jamieliu2@users.noreply.github.com>
2026-01-28 13:51:41 -0800

    Update pltstyle.py to increase linewidth

--- pltstyle.py
+++ pltstyle.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-v0_8')
 plt.rcParams['font.family'] = 'monospace'
-plt.rcParams['lines.linewidth'] = 0.5
+plt.rcParams['lines.linewidth'] = 2
 plt.rcParams['lines.markersize'] = 3
